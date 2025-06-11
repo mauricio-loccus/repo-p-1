97699e7fbe7be2201691b005d130f9dc8356fd82
Mauricio Lima <mauricio.lima@loccus.com.br>
2025-06-11 00:40:13 -0300

    Adição de estado de estado de duas formas - O estado nesse momento será representado pela data de um conjunto de   arquivos que nesse caso é incluido o arquivo 'main.css' que não   existe e faz parte dos testes para ilustrar o tratamento de uma   condição de erro. - As duas formas é que inicialmente foi feita a requisição de cada   arquivo e uma latência perceptível foi notada e foi feito de maneira   paralela, para observar a possibilidade de agilizar o processo.

--- main.js
+++ main.js
@@ -1,9 +1,134 @@
 ((global) => {
 
-    function DOMLoaded()
+    async function DOMLoaded()
+    {
+        updateView()
+    }
+
+
+    async function updateView()
     {
         document.querySelector('#date-time').innerText = (new Date()).toLocaleTimeString()
+
+        for(const index of [ 'paralell','sequential' ])
+        {
+            const result = { value : null }
+
+            const start = Date.now()
+            switch (index)
+            {
+                
+                case 'sequential':
+                    result.value = await sequential_getState()
+                    break
+
+                case 'paralell':
+                    result.value = await paralell_getState()
+                    break
+
+            }
+            document.querySelector(`#state-${index}`).innerText = result.value ? (JSON.stringify(result.value, null, 4)) : ''
+            
+            document.querySelector(`#interval-${index}`).innerText = (Date.now() - start) / 1000
+        }
+        
     }
+
+
+    async function paralell_getState()
+    {
+        const states = [
+            {
+                url        : 'index.html',
+                lastUpdate :  null
+            },
+            {
+                url        : 'main.css',
+                lastUpdate :  null
+            },
+            {
+                url        : 'main.js',
+                lastUpdate :  null
+            }
+        ]
+
+        const results = await Promise.allSettled(states.map( state => new Promise( async (resolve,reject) => {
+            try
+            {
+                const response = await fetch(state.url, { method: 'HEAD' })
+                if (!response.ok)
+                    throw new Error('Fail on fetch')
+
+                const lastModified = response.headers.get('Last-Modified')
+                if (!lastModified)
+                    throw new Error('Invalid Value')
+
+                const lastModifiedTime = new Date(lastModified);
+                if (isNaN(lastModifiedTime.getTime()))
+                    throw new Error('Invalid Date')
+
+                state.lastUpdate = lastModifiedTime
+
+                resolve(state)
+            }
+            catch (exception)
+            {
+                reject({
+                    ...state,
+                    error : exception.message
+                })
+            }
+        })))
+
+        //return results
+        return results.map( result => result.status == 'fulfilled' ? ( result.value ) : ( result.status == 'rejected' ? result.reason : null ) )
+    }
+
+
+    async function sequential_getState()
+    {
+        const states = [
+            {
+                url        : 'index.html',
+                lastUpdate :  null
+            },
+            {
+                url        : 'main.css',
+                lastUpdate :  null
+            },
+            {
+                url        : 'main.js',
+                lastUpdate :  null
+            }
+        ]
+
+        for(const state of states)
+        {
+            try
+            {
+                const response = await fetch(state.url, { method: 'HEAD' })
+                if (!response.ok)
+                    throw new Error('Fail on fetch')
+
+                const lastModified = response.headers.get('Last-Modified')
+                if (!lastModified)
+                    throw new Error('Invalid Value')
+
+                const lastModifiedTime = new Date(lastModified);
+                if (isNaN(lastModifiedTime.getTime()))
+                    throw new Error('Invalid Date')
+
+                state.lastUpdate = lastModifiedTime
+            }
+            catch (exception)
+            {
+                state.error = exception.message
+            }
+        }
+
+        return states
+    }
+
 
     document.addEventListener('DOMContentLoaded', DOMLoaded)
 
